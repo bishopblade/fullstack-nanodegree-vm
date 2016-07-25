26a3e4e20380b8408a52758261409a53fef1e6d0
Rowan Tran <rowanjames.tran@gmail.com>
2016-07-25 16:54:20 -0700

    First commit

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -7,21 +7,38 @@ import psycopg2
 
 
 def connect():
-    """Connect to the PostgreSQL database.  Returns a database connection."""
-    return psycopg2.connect("dbname=tournament")
+    """Connect to the PostgreSQL database.  Returns a database connection and cursor."""
+    conn = psycopg2.connect("dbname=tournament")
+    cursor = conn.cursor()
 
+    return (conn, cursor)
+
+def end(conn, cursor):
+    """Commit changes to database and close cursor and connection."""
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 def deleteMatches():
     """Remove all the match records from the database."""
-
+    conn, cursor = connect()
+    cursor.execute("TRUNCATE TABLE matches")
+    end(conn, cursor)
 
 def deletePlayers():
     """Remove all the player records from the database."""
-
+    conn, cursor = connect()
+    cursor.execute("TRUNCATE TABLE players CASCADE")
+    end(conn, cursor)
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    conn, cursor = connect()
+    cursor.execute("SELECT count(*) FROM players")
+    count = cursor.fetchone()[0]
+    end(conn, cursor)
 
+    return count
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
@@ -32,7 +49,9 @@ def registerPlayer(name):
     Args:
       name: the player's full name (need not be unique).
     """
-
+    conn, cursor = connect()
+    cursor.execute("INSERT INTO players (name) VALUES (%s)", (name,))
+    end(conn, cursor)
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
@@ -47,7 +66,12 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    conn, cursor = connect()
+    cursor.execute("SELECT * FROM standings")
+    standings = cursor.fetchall()
+    end(conn, cursor)
 
+    return standings
 
 def reportMatch(winner, loser):
     """Records the outcome of a single match between two players.
@@ -56,7 +80,9 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
+    conn, cursor = connect()
+    cursor.execute("INSERT INTO matches (winner, loser) VALUES (%s, %s)", (winner, loser))
+    end(conn, cursor)
  
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
@@ -73,5 +99,10 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
-
+    standings = playerStandings() 
+    matchups = []
+    for i in range(0, len(standings) - 1, 2):
+        first = standings[i]
+        second = standings[i+1]
+        matchups.append((first[0], first[1], second[0], second[1]))
+    return matchups
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,28 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE TABLE players (
+    id serial PRIMARY KEY,
+    name varchar(255)
+);
 
+CREATE TABLE matches (
+    id serial PRIMARY KEY,
+    winner integer REFERENCES players (id),
+    loser integer REFERENCES players (id)
+);
+
+CREATE VIEW player_wins AS
+SELECT players.*, count(matches.winner) AS wins
+FROM players LEFT JOIN matches ON (players.id = matches.winner)
+GROUP BY players.id;
+
+CREATE VIEW player_losses AS
+SELECT players.*, count(matches.loser) AS losses
+FROM players LEFT JOIN matches ON (players.id = matches.loser)
+GROUP BY players.id;
+
+CREATE VIEW standings AS
+SELECT player_wins.*, player_wins.wins + player_losses.losses AS matches
+FROM player_wins JOIN player_losses ON (player_wins.id = player_losses.id)
+ORDER BY wins DESC;
